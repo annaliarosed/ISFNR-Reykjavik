db72ef28b52bacf823b80d0216086be019a870b3
Annalia DeStefano <annaliadestefano@gmail.com>
2025-08-20 16:42:31 +0300

    working programme on only programme page

--- src/components/Programme/Programme.jsx
+++ src/components/Programme/Programme.jsx
@@ -1,66 +1,22 @@
-// // import React from "react";
-
-// // const Programme = () => {
-// //   return (
-// //     <div>
-// //       <div style={{ height: "100vh" }}>
-// //         <iframe
-// //           src="https://nomadit.co.uk/conference/isfnr2026/explorer"
-// //           style={{ width: "100%", height: "100%", border: "none" }}
-// //           title="Conference Programme"
-// //         />
-// //       </div>
-// //     </div>
-// //   );
-// // };
-
-// // export default Programme;
-// import React, { useEffect } from "react";
-
-// const Programme = () => {
-//   useEffect(() => {
-//     const container = document.getElementById("react-minisite-wrapper");
-
-//     if (!container) return;
-
-//     // Remove old explorer if re-rendered
-//     const oldExplorer = document.getElementById("conference-sweet");
-//     if (oldExplorer) oldExplorer.remove();
-
-//     // Create new explorer div
-//     const sweetDiv = document.createElement("div");
-//     sweetDiv.id = "conference-sweet";
-//     sweetDiv.dataset.conference = "isfnr2026";
-//     sweetDiv.style.minHeight = "100vh";
-//     sweetDiv.innerHTML = "Loading conference panels ...";
-
-//     container.appendChild(sweetDiv);
-
-//     // Trigger setup
-//     if (typeof window.SetupConferenceExplorer === "function") {
-//       window.SetupConferenceExplorer();
-//     } else {
-//       console.warn("SetupConferenceExplorer is not defined");
-//     }
-//   }, []);
-
-//   return (
-//     <div id="react-minisite-wrapper">
-//       <h1>Programme</h1>
-//       {/* Explorer will be injected here */}
-//     </div>
-//   );
-// };
 import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const Programme = () => {
+  const location = useLocation();
+
   useEffect(() => {
     const CONFERENCE_TAG = "isfnr2026";
 
-    // Don’t reinject if already present
-    if (document.getElementById("conference-sweet")) return;
+    const isProgrammePage =
+      location.hash === "#/programme" ||
+      location.pathname.endsWith("/programme");
 
-    // Add the DOM element for the explorer
+    if (!isProgrammePage) return;
+
+    const sweetDivExists = document.getElementById("conference-sweet");
+    if (sweetDivExists) return;
+
+    // Inject the explorer container
     const sweetDiv = document.createElement("div");
     sweetDiv.id = "conference-sweet";
     sweetDiv.dataset.conference = CONFERENCE_TAG;
@@ -68,9 +24,11 @@ const Programme = () => {
     sweetDiv.innerHTML = "Loading conference panels…";
 
     const container = document.getElementById("react-minisite-wrapper");
-    if (container) container.appendChild(sweetDiv);
+    if (container) {
+      container.appendChild(sweetDiv);
+    }
 
-    // Inject the explorer script (must be this specific one!)
+    // Load the script only once
     const script = document.createElement("script");
     script.src = "https://nomadit.co.uk/conference/conference-page-loader.js";
     script.async = true;
@@ -83,7 +41,13 @@ const Programme = () => {
     };
 
     document.body.appendChild(script);
-  }, []);
+
+    // Optional: cleanup if user navigates away
+    return () => {
+      const div = document.getElementById("conference-sweet");
+      if (div) div.remove();
+    };
+  }, [location]);
 
   return (
     <div id="react-minisite-wrapper">
